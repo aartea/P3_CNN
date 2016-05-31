8c025b1824b878c526f0020f7c841258c7dbd1b1
aaron <aarongitis@gmail.com>
2016-05-31 10:39:13 -0700

    Add JSON class handler

---
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/GetCurrentDateTime.java
@@ -0,0 +1,14 @@
+package badassapps.aaron.newshag;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by aaron on 5/31/2016.
+ */
+public class GetCurrentDateTime {
+    GetCurrentDateTime(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    }
+}
---
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/GuardianAppData.java
@@ -0,0 +1,11 @@
+package badassapps.aaron.newshag;
+
+/**
+ * Created by aaron on 5/31/2016.
+ */
+
+
+public class GuardianAppData {
+
+
+}
