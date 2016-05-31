1b3f8b82969f2f1d6b7ed7abda302a26061956dd
aharshbe <a.catawampus@gmail.com>
2016-05-31 10:25:10 -0700

    Adding the notifcation logic to onCreate

--- NewsHag/app/src/main/java/badassapps/aaron/newshag/MainActivity.java
+++ NewsHag/app/src/main/java/badassapps/aaron/newshag/MainActivity.java
@@ -45,6 +45,62 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Toast.makeText(getApplicationContext(), "Connection ready",
+                    Toast.LENGTH_SHORT).show();
+
+            Intent intent1 = new Intent(this, MainActivity.class);
+
+
+            PendingIntent pendingIntent1 = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent1, 0);
+
+
+            NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
+            bigPictureStyle.bigPicture(BitmapFactory.decodeResource(getResources(), R.drawable.ic_menu_gallery)).build();
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+            mBuilder.setSmallIcon(android.R.drawable.star_on);
+            mBuilder.setContentTitle("Connection Good!");
+            mBuilder.setContentText("Get SURFIN");
+            mBuilder.setContentIntent(pendingIntent1);
+            mBuilder.setPriority(Notification.PRIORITY_MAX);
+            mBuilder.setStyle(bigPictureStyle);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(1, bigPictureStyle.build());
+            notificationManager.cancel(6);
+
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Connection not ready",
+                    Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent1 = new Intent(Settings.ACTION_WIFI_SETTINGS);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+            PendingIntent pendingIntent1 = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent1, 0);
+
+
+            NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
+            bigPictureStyle.bigPicture(BitmapFactory.decodeResource(getResources(), R.drawable.ic_menu_slideshow)).build();
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+            mBuilder.setSmallIcon(android.R.drawable.stat_sys_warning);
+            mBuilder.setContentTitle("No internet connection!");
+            mBuilder.setContentText("To use the app, please enable WIFI, Thanks!");
+            mBuilder.setContentIntent(pendingIntent);
+            mBuilder.setPriority(Notification.PRIORITY_MAX);
+            mBuilder.setStyle(bigPictureStyle);
+            mBuilder.addAction(android.R.drawable.ic_menu_info_details, "Connect WIFI", pendingIntent1);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(6, bigPictureStyle.build());
+            notificationManager.cancel(1);
+
+
+        }
     }
 
     @Override
@@ -104,66 +160,4 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public void checkingconnection(View view) {
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            Toast.makeText(getApplicationContext(), "Connection ready",
-                    Toast.LENGTH_SHORT).show();
-
-            Intent intent1 = new Intent(this, MainActivity.class);
-
-
-            PendingIntent pendingIntent1 = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent1, 0);
-
-
-
-            NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
-            bigPictureStyle.bigPicture(BitmapFactory.decodeResource(getResources(),R.drawable.ic_menu_gallery)).build();
-
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-            mBuilder.setSmallIcon(android.R.drawable.star_on);
-            mBuilder.setContentTitle("Connection Good!");
-            mBuilder.setContentText("Get SURFIN");
-            mBuilder.setContentIntent(pendingIntent1);
-            mBuilder.setPriority(Notification.PRIORITY_MAX);
-            mBuilder.setStyle(bigPictureStyle);
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(1, bigPictureStyle.build());
-            notificationManager.cancel(6);
-
-
-
-
-        } else {
-            Toast.makeText(getApplicationContext(), "Connection not ready",
-                    Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, MainActivity.class);
-            Intent intent1 = new Intent(Settings.ACTION_WIFI_SETTINGS);
-
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
-            PendingIntent pendingIntent1 = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent1, 0);
-
-
-
-
-            NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
-            bigPictureStyle.bigPicture(BitmapFactory.decodeResource(getResources(),R.drawable.ic_menu_slideshow)).build();
-
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-            mBuilder.setSmallIcon(android.R.drawable.stat_sys_warning);
-            mBuilder.setContentTitle("No internet connection!");
-            mBuilder.setContentText("To use the app, please enable WIFI, Thanks!");
-            mBuilder.setContentIntent(pendingIntent);
-            mBuilder.setPriority(Notification.PRIORITY_MAX);
-            mBuilder.setStyle(bigPictureStyle);
-            mBuilder.addAction(android.R.drawable.ic_menu_info_details, "Connect WIFI", pendingIntent1);
-
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(6, bigPictureStyle.build());
-            notificationManager.cancel(1);
-
-
-        }
-    }
 }
